900181744e08ebd44a2c078741dd21ba6290e2d8
oedm <git@moed.cc>
2018-02-18 15:51:57 +0100

    Changing tfstate destition in S3 bucket from testing to stage

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-dng"
-    key = "testing/terraform.tfstate"
+    key = "stage/terraform.tfstate"
     region = "eu-west-1"
     dynamodb_table = "terraform-lock"
   }
